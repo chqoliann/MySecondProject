5781db862e5fe2a9bdd62887142fc80c0bc410ee
Narek Chqolyan <159801789+chqoliann@users.noreply.github.com>
2024-05-06 11:29:16 +0400

    Delete Webex_proj directory

--- Webex_proj/Webex_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, signup_view, signin_view
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('signup/', signup_view, name='signup'),
-    path('signin/', signin_view, name='signin')
-]
